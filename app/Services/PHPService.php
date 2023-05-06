343d239f937b136485d4695082bbcd2e630bd6b9
Nyamort <rmurat.murat@gmail.com>
2023-05-06 17:02:38 +0200

    fix

--- app/Services/PHPService.php
+++ app/Services/PHPService.php
@@ -43,25 +43,21 @@ class PHPService
     public function installVersion(string $version): void
     {
         $this->commandService->run("apt install -y php$version");
-        $this->commandService->run("dpkg --configure -a");
     }
 
     public function installExtension(string $version, string $extension): void
     {
         $this->commandService->run("apt install -y php$version-$extension");
-        $this->commandService->run("dpkg --configure -a");
     }
 
     public function uninstallVersion(string $version): void
     {
         $this->commandService->run("apt remove -y php$version");
-        $this->commandService->run("dpkg --configure -a");
     }
 
     public function uninstallExtension(string $version, string $extension): void
     {
         $this->commandService->run("apt remove -y php$version-$extension");
-        $this->commandService->run("dpkg --configure -a");
     }
 
     public function uninstallVersionAndExtensions(string $version): void
